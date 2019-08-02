ced3b9dbe2644ca3d1049b7035ff16fa24bb168b
Keith Thor <keithkeethor@gmail.com>
2019-08-01 18:42:16 -0700

    Implement carousel component
    Implemented the carousel component that is displayed when a user clicks
    on a hero image for any piece of furniture. The click listener is
    attached to the hero image in the component-factory. Clicking on the
    carousel overlay will exit the carousel component.

--- assets/js/carousel-factory/carousel-factory.js
+++ assets/js/carousel-factory/carousel-factory.js
@@ -1,5 +1,74 @@
-const make = () => {
-    
+/**Makes a carousel component for a single furniture item. */
+const make = (furnitureData) => {
+    let carouselRoot = document.createDocumentFragment();
+
+    let overlay = document.createElement("div");
+    overlay.classList = "product-carousel-overlay";
+    // Exit carousel when the overlay is clicked
+    overlay.addEventListener("click", () => overlay.parentElement.removeChild(overlay));
+    carouselRoot.appendChild(overlay);
+
+    let carouselContainer = document.createElement("div");
+    carouselContainer.classList = "product-carousel";
+    overlay.appendChild(carouselContainer);
+
+    let thumbnailContainer = document.createElement("div");
+    thumbnailContainer.classList = "product-thumbnail-container";
+    carouselContainer.appendChild(thumbnailContainer);
+
+    let mainView = makeMainView(furnitureData.hero);
+    carouselContainer.appendChild(mainView);
+
+    // Thumbnail for hero image not included in list of images
+    let heroThumb = makeThumbnail(furnitureData.thumbnail);
+
+    // On click, change main view image to that of the clicked thumbnail image
+    heroThumb.addEventListener("click", (event) => {
+        mainView.children[0].src = heroThumb.children[0].src;
+        mainView.children[0].alt = heroThumb.children[0].alt;
+        event.stopPropagation();
+    });
+    thumbnailContainer.appendChild(heroThumb);
+
+    furnitureData.images.forEach(image => {
+        let thumbnail = makeThumbnail(image);
+        thumbnail.addEventListener("click", () => {
+            mainView.children[0].src = thumbnail.children[0].src;
+            mainView.children[0].alt = thumbnail.children[0].alt;
+            event.stopPropagation();
+        });
+        thumbnailContainer.appendChild(thumbnail);
+    });
+
+    return carouselRoot;
 };
+
+/**Makes an image element for the selected image view. */
+const makeMainView = (imageData) => {
+    let viewContainer = document.createElement("div");
+    viewContainer.classList = "product-carousel-view";
+
+    let viewImage = document.createElement("img");
+    viewImage.src = imageData.href;
+    viewImage.alt = imageData.alt;
+
+    viewContainer.appendChild(viewImage);
+
+    return viewContainer;
+}
+
+/**Makes a thumbnail element for a single provided image data. */
+const makeThumbnail = (imageData) => {
+    let thumbnailContainer = document.createElement("div");
+    thumbnailContainer.classList = "product-carousel-thumbnail";
+
+    let thumbnail = document.createElement("img");
+    thumbnail.src = imageData.href;
+    thumbnail.alt = imageData.alt;
+
+    thumbnailContainer.appendChild(thumbnail);
+
+    return thumbnailContainer;
+}
 
 export default { make };
--- assets/js/display-builder/display-builder.js
+++ assets/js/display-builder/display-builder.js
@@ -1,12 +1,25 @@
 import componentFactory from "../component-factory/component-factory";
+import carouselFactory from "../carousel-factory/carousel-factory";
 
-/**Builds the components for every piece of furniture in the provided json data. */
+/**Builds the components for every piece of furniture in the provided json data and returns a documentFragment containing
+ * the elements. */
 const build = (data) => {
     let fragment = document.createDocumentFragment();
     let furnitureData = data.groups;
 
     furnitureData.forEach(furniture => {
-        fragment.appendChild(componentFactory.make(furniture));
+        let component = componentFactory.make(furniture);
+
+        // Add event listener to hero image to show carousel
+        let heroImg = component.querySelector(".product-hero");
+        heroImg.addEventListener("click", () => {
+            let carouselFragment = carouselFactory.make(furniture);
+            let carouselElement = document.getElementById("carousel");
+            carouselElement.innerHTML = "";
+            carouselElement.appendChild(carouselFragment);
+        });
+
+        fragment.appendChild(component);
     });
 
     return fragment;
